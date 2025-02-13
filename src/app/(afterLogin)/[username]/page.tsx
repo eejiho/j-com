a706e622b61a877ae4e11a34c211750e80c3ec54
jiho <jihoxe@gmail.com>
2025-02-13 15:56:32 +0900

    좌/우측 섹션 & 홈 탭바 클론코딩

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   return (
     <main>
-
+      프로필페이지
     </main>
   );
 }
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,9 +1,26 @@
+import Post from '@/app/(afterLogin)/_component/Post';
+import PostForm from './_component/PostForm';
+import Tab from './_component/Tab';
+import style from './home.module.css';
+import TabProvider from './_component/TabProvider';
+
 export default function Home() {
   return (
-    <div>
-        <main>
-            홈페이지
-        </main>
-    </div>
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
   );
 }
