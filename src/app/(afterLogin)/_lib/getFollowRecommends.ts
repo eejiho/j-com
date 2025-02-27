c009c645b87f0de905dfcfd6d2da1d296ff02684
jiho <jihoxe@gmail.com>
2025-02-27 17:00:42 +0900

    encodeURIComponent 반영

--- src/app/(afterLogin)/_lib/getFollowRecommends.ts
+++ src/app/(afterLogin)/_lib/getFollowRecommends.ts
@@ -2,7 +2,8 @@ export async function getFollowRecommends() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/followRecommends`, {
       next: {
         tags: ['users', 'followRecommends'],
-      }
+      },
+      credentials: 'include',
     });
   
     if (!res.ok) {
