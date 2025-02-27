2f2fb18f9fa77a428afd0693b32bcc901d44cb05
jiho <jihoxe@gmail.com>
2025-02-27 14:57:27 +0900

    로그아웃, 로그인 실제로 하기

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/upload/:slug',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug`,
+      },
+    ]
+  }
 };
 
 export default nextConfig;
